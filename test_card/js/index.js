1dd560592226890c7634343a8dd15d1c5403a76e
LOLCR4FT <66711995+LOLCR4FT@users.noreply.github.com>
2021-11-10 14:34:30 +0100

    Update index.js

--- test_card/js/index.js
+++ test_card/js/index.js
@@ -10,22 +10,3 @@ $('.btn-style').click(function(ev){
     $(this).addClass('active');
   }
 });
-
-
-let jqxhr = $.ajax( "example.php" )
-  .done(function() {
-    alert( "success" );
-  })
-  .fail(function() {
-    alert( "error" );
-  })
-  .always(function() {
-    alert( "complete" );
-  });
- 
-// Perform other work here ...
- 
-// Set another completion function for the request above
-jqxhr.always(function() {
-  alert( "second complete" );
-});
